5d66bbdac2469fed91e1e45f43e93f6d11f2c754
xianding <xianding.wang@monstar-lab-cn.com>
2017-11-21 14:03:20 +0800

    添加退款测试未完成

---
+++ src/main/java/cn/monstar/payment/web/controller/form/RefundsFrom.java
@@ -0,0 +1,78 @@
+package cn.monstar.payment.web.controller.form;
+
+import java.io.Serializable;
+
+/**
+ * @author wangxianding
+ * @version 1.0
+ * @description 退款请求Form
+ * @date 2017/11/21 上午9:50
+ */
+public class RefundsFrom implements Serializable{
+
+	/**
+	 * 平台交易流水号
+	 */
+	private String paymentNo;
+
+	/**
+	 * 支付总金额
+	 */
+	private String orderMoney;
+
+	/**
+	 * 退款金额
+	 */
+	private String refundMoney;
+
+	/**
+	 * 退款原因
+	 */
+	private String refundDescription;
+
+	/**
+	 * 退款流水号
+	 */
+	private String refundNo;
+
+
+	public String getPaymentNo() {
+		return paymentNo;
+	}
+
+	public void setPaymentNo(String paymentNo) {
+		this.paymentNo = paymentNo;
+	}
+
+	public String getOrderMoney() {
+		return orderMoney;
+	}
+
+	public void setOrderMoney(String orderMoney) {
+		this.orderMoney = orderMoney;
+	}
+
+	public String getRefundMoney() {
+		return refundMoney;
+	}
+
+	public void setRefundMoney(String refundMoney) {
+		this.refundMoney = refundMoney;
+	}
+
+	public String getRefundDescription() {
+		return refundDescription;
+	}
+
+	public void setRefundDescription(String refundDescription) {
+		this.refundDescription = refundDescription;
+	}
+
+	public String getRefundNo() {
+		return refundNo;
+	}
+
+	public void setRefundNo(String refundNo) {
+		this.refundNo = refundNo;
+	}
+}
--- src/test/java/cn/monstar/payment/test/controller/RefundsControllerTest.java
+++ src/test/java/cn/monstar/payment/test/controller/RefundsControllerTest.java
@@ -1,23 +1,39 @@
 package cn.monstar.payment.test.controller;
 
 
+import cn.monstar.payment.config.TestsConfiguration;
+import cn.monstar.payment.domain.model.dto.APIResultDto;
+import cn.monstar.payment.web.controller.form.RefundsFrom;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestsConfiguration.class)
 public class RefundsControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @Test
-    public void submitRefunds(){
-        //testRestTemplate.postForEntity("", );
-    }
+//    @Test
+//    public void submitRefunds(){
+//        RefundsFrom refundsFrom = new RefundsFrom();
+//        refundsFrom.setOrderMoney("1");
+//        refundsFrom.setRefundMoney("0.5");
+//        refundsFrom.setPaymentNo("123456");
+//        refundsFrom.setRefundNo("666666");
+//        refundsFrom.setRefundDescription("退款原因");
+//
+//        APIResultDto apiResultDto = testRestTemplate.postForObject("/payment/refunds/sendRefund", refundsFrom, APIResultDto.class);
+//        Assert.assertEquals(apiResultDto.getReturnCode(), 0);
+//        Assert.assertNotNull(apiResultDto.getData());
+//        Assert.assertNotNull(apiResultDto.getData());
+//    }
 
 }
