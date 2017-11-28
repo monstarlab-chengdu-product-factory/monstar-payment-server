5903fdf53758aa97bdd59cff32f015a1dffd93ac
xianding <xianding.wang@monstar-lab-cn.com>
2017-11-28 17:40:40 +0800

    添加沙箱开关

---
+++ src/main/java/cn/monstar/payment/config/MonstarConfig.java
@@ -0,0 +1,28 @@
+package cn.monstar.payment.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangxianding
+ * @version 1.0
+ * @description
+ * @date 2017/11/28 下午1:36
+ */
+@Component
+@ConfigurationProperties(prefix = "monstar")
+public class MonstarConfig {
+
+    /**
+     * 是否启用沙箱环境
+     */
+    private boolean sandboxnew;
+
+    public void setSandboxnew(Boolean sandboxnew) {
+        this.sandboxnew = sandboxnew;
+    }
+
+    public Boolean getSandboxnew() {
+        return sandboxnew;
+    }
+}
