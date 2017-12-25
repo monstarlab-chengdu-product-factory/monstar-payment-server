ce5f9a8a34271e83649ac8f55f3e8faef6d40de2
xianding <xianding.wang@monstar-lab-cn.com>
2017-12-25 16:53:07 +0800

    添加微信二维码生成接口

--- src/main/java/cn/monstar/payment/domain/util/StringUtil.java
+++ src/main/java/cn/monstar/payment/domain/util/StringUtil.java
@@ -1,6 +1,9 @@
 package cn.monstar.payment.domain.util;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -102,6 +105,43 @@ public class StringUtil {
      */
     public static String getNonceStr() {
         return getRandomString(32, RANDOM_MIX);
+    }
+
+    /**
+     * 获取客户端ip
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1")) {
+                // 根据网卡取本机配置的IP
+                // get IP from network card
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP，多个IP按照','分割
+        // if has many proxy, get the fist IP,
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
     }
 
 
